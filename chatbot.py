2c00241e15ba148493e3cbd3b2206fc29a0249a7
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-09-24 19:51:41 -0700

    I have created  variables and  a function

---
+++ chatbot.py
@@ -0,0 +1,12 @@
+from random import choice
+
+human_respond = input("what kind of joke you want to hear? dad joke or  a funny joke: ")
+
+def joke_bot_response(human_response):
+    response_dad_joke = ["If you see a crime at an Apple Store does that make you an iWitness!", 
+    "What’s an astronaut’s favorite part of a computer The space bar.", 
+    "Wanna hear a joke about paper? Never mind—it's tearable."]
+    response_funny_joke = ["How do you have a major in finance and be broke?", 
+    "A communist joke isn't funny unless everyone gets it", 
+    "What do you call a psychic little person who has escaped from prison? A small medium at large"] 
+
